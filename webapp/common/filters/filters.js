6f3ca50bfaf65592124bb5f873c67fa94895c782
eugenenew80 <eugenenew80@gmail.com>
2017-12-26 15:31:19 +0600

    -

--- webapp/common/filters/filters.js
+++ webapp/common/filters/filters.js
@@ -190,6 +190,10 @@
 		  };
 		})
 
+        .filter('unsafe', function($sce) {
+            return function(val) {
+                return $sce.trustAsHtml(val);
+            };
+        })
 	;
-				
-})();	
+})();
--- webapp/dict/features/dictPowerLine/models/dictPowerLineDescriptionService.js
+++ webapp/dict/features/dictPowerLine/models/dictPowerLineDescriptionService.js
@@ -344,7 +344,7 @@
 
             				fieldBuilder.build({
             					name: "po",
-            					labelDesc: "Удельная емкостная проводимость, x10e-6 См/км",
+            					labelDesc: "Удельная емкостная проводимость, x10&#8310; См/км",
                                 labelClass: "col-sm-8",
                                 controlClass: "col-sm-4",
                                 controlDataType: "number",
