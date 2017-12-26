dc440752260c3b200f8412eed47c8cb620fe326e
eugenenew80 <eugenenew80@gmail.com>
2017-12-26 15:39:51 +0600

    -

--- webapp/common/directives/complexFormControl/control/control.js
+++ webapp/common/directives/complexFormControl/control/control.js
@@ -78,6 +78,7 @@ angular.module("common")
 						scope.templateURL=scope.templateURL + "stringInput.html";
 				}
 
+                scope.templateURL = scope.templateURL + "?version=v4";
 
 				//Bootstrap UI datepicker options
 				scope.datepicker={
